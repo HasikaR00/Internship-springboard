aacdc4006dafb32498f8a6d758dc9e99fbfa166a
HasikaR00 <145417516+HasikaR00@users.noreply.github.com>
2024-12-20 17:11:58 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import ReactDOM from "react-dom/client";  // Note: react-dom/client for React 18+
+import ReactDOM from "react-dom/client";  
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-// Create a root element for rendering
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-// Render the App component inside the root element
+
 root.render(
   <React.StrictMode>
     <App />
