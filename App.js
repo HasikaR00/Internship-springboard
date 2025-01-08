4230d9a4d76596c15ce96dec3ff578830fe32c61
HasikaR00 <145417516+HasikaR00@users.noreply.github.com>
2025-01-08 18:30:51 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import Signup from "./components/signup";
-import Login from "./components/login";
-import ForgetPassword from "./components/forgetpassword";
-import "./components/styles.css";
+import Signup from "./signup";
+import Home from "./home";
+import ApprovalDashboard from "./approvaldashboard";
+import Login from "./login";
+import HRDashboard from "./hrdashboard";
+import InstructorDashboard from "./instructordashboard";
+import ForgetPassword from "./forgetpassword";
+import "./styles.css";
+import LearnerDashboard from "./learnerdashboard";
+import ManagerDashboard from "./managerdashboard";
 
 const App = () => {
   return (
@@ -11,7 +17,13 @@ const App = () => {
       <Routes>
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/approvaldashboard" element={<ApprovalDashboard />} />
         <Route path="/forgetpassword" element={<ForgetPassword />} />
+        <Route path="/hrdashboard" element={<HRDashboard />} /> 
+        <Route path="/instructordashboard" element={<InstructorDashboard />} /> 
+        <Route path="/learnerdashboard" element={<LearnerDashboard />} /> 
+        <Route path="/managerdashboard" element={<ManagerDashboard />} /> 
         <Route path="/" element={<Navigate to="/login" replace />} />
       </Routes>
     </Router>
@@ -19,3 +31,4 @@ const App = () => {
 };
 
 export default App;
+
